5210f78360160c01879e42fc011cb7243562474b
Kgray44 <64373011+Kgray44@users.noreply.github.com>
2020-04-28 07:18:34 -0400

    Update settings.h

--- settings.h
+++ settings.h
@@ -25,6 +25,6 @@ char WIFI_PASS[] = "your_pass";
 #define maxActuatorStroke 16  //max stroke of linear actuator
 #define percentOfMaxStroke 1  //0 through 1
 #define a1 2                  //first linear actuator
-#define a2 3                  //first linear actuator
+#define a2 0                  //first linear actuator
 #define b1 4                  //second linear actuator
 #define b2 5                  //second linear actuator
