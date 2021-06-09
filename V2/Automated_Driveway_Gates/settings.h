972d2bd0e5f6d77924cd24cd5374fc3d82925c37
K Gray <64373011+Kgray44@users.noreply.github.com>
2021-06-09 10:26:47 -0400

    Create settings.h

---
+++ V2/Automated_Driveway_Gates/settings.h
@@ -0,0 +1,29 @@
+#define emergencyButton 14
+
+//blynk
+#define BLYNK_AUTH_TOKEN " xRH8NnsHrOGi9utKIbLg0pp5Q5gaHtE9"
+#define BLYNK_GREEN     "#23C48E"
+#define BLYNK_RED       "#D3435C"
+#define BLYNK_YELLOW    "#ED9D00"
+#define BLYNK_BLUE      "#04C0F8"
+#define YOUR_LAT        43.419868
+#define YOUR_LONG       -72.467632
+
+//WiFi
+char WIFI_SSID[] = "Apple Network 85064d";
+char WIFI_PASS[] = "12344321";
+
+//GATE
+String Mode = "button";      //automatic or button
+#define gateNumber 2          //how many gates; 1 or 2
+int gateOpenAmount = 10;     //only for automatic mode, seconds
+#define gateDirection 1       //1 or 2
+
+//linear actuator
+#define inchesPerSec .39      //inches per sec of linear actuator
+#define maxActuatorStroke 12  //max stroke of linear actuator
+#define percentOfMaxStroke .21 //.2 //0 through 1
+#define a1 2                  //first linear actuator
+#define a2 0                  //first linear actuator
+#define b1 4                  //second linear actuator
+#define b2 5                  //second linear actuator
