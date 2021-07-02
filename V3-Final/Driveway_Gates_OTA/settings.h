87b5d00e27a9868a7cd4d132fa4d8a533420c023
K Gray <64373011+Kgray44@users.noreply.github.com>
2021-07-02 01:12:05 -0400

    Update settings.h

--- V3-Final/Driveway_Gates_OTA/settings.h
+++ V3-Final/Driveway_Gates_OTA/settings.h
@@ -29,3 +29,22 @@ int gateOpenAmount = 10;      //only for automatic mode, seconds
 #define a2 D4                  //first linear actuator
 #define b1 D5                  //second linear actuator
 #define b2 D6                  //second linear actuator
+
+#include "AdafruitIO_WiFi.h"
+
+#if defined(USE_AIRLIFT) || defined(ADAFRUIT_METRO_M4_AIRLIFT_LITE) ||         \
+    defined(ADAFRUIT_PYPORTAL)
+// Configure the pins used for the ESP32 connection
+#if !defined(SPIWIFI_SS) // if the wifi definition isnt in the board variant
+// Don't change the names of these #define's! they match the variant ones
+#define SPIWIFI SPI
+#define SPIWIFI_SS 10 // Chip select pin
+#define NINA_ACK 9    // a.k.a BUSY or READY pin
+#define NINA_RESETN 6 // Reset pin
+#define NINA_GPIO0 -1 // Not connected
+#endif
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS, SPIWIFI_SS,
+                   NINA_ACK, NINA_RESETN, NINA_GPIO0, &SPIWIFI);
+#else
+AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
+#endif
